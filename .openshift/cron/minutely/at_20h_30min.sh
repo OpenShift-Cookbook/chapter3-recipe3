4c756e5aec1df422b16153a5ef3d2d93fc4ab494
Shekhar Gulati <shekhargulati84@gmail.com>
2014-02-04 16:10:49 +0530

    run at 20:30 pm

--- .openshift/cron/minutely/at_20h_30min.sh
+++ .openshift/cron/minutely/at_20h_30min.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ `date +%H:%M` == "05:33" ]
+if [ `date +%H:%M` == "20:30" ]
 then
     date >> $OPENSHIFT_REPO_DIR/php/quota_20h_30m.txt
 	quota -s >> $OPENSHIFT_REPO_DIR/php/quota_20h_30m.txt
