8812fe98ea74e1f4c093907ea7d09a194ab8c31b
Shekhar Gulati <shekhargulati84@gmail.com>
2014-02-04 13:26:14 +0530

    added seperator between runs

--- .openshift/cron/minutely/quota.sh
+++ .openshift/cron/minutely/quota.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 date >> $OPENSHIFT_REPO_DIR/php/quota.txt
 quota -s >> $OPENSHIFT_REPO_DIR/php/quota.txt
-echo "***********************************************" >> $OPENSHIFT_REPO_DIR/php/quota.txt
+echo "**********************************************************************************************" >> $OPENSHIFT_REPO_DIR/php/quota.txt
