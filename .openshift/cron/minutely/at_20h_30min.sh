3b064ac6ae6ecd8c39ee1aa4c43992ad9986e463
Shekhar Gulati <shekhargulati84@gmail.com>
2014-02-04 16:01:26 +0530

    added script to run at specific time

--- .openshift/cron/minutely/at_20h_30min.sh
+++ .openshift/cron/minutely/at_20h_30min.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ `date +%H:%M` == "16:01" ]
+if [ `date +%H:%M` == "05:33" ]
 then
     date >> $OPENSHIFT_REPO_DIR/php/quota_20h_30m.txt
 	quota -s >> $OPENSHIFT_REPO_DIR/php/quota_20h_30m.txt
